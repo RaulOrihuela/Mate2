dc3a258de6b2578c0b971f9389ec04f84e985304
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2019-04-25 21:17:50 -0500

    Cambios en el codigo completo.

--- videoExample.py
+++ videoExample.py
@@ -1,56 +1,45 @@
-import numpy as np
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-debug = False
-cannyMin = 125   
+cannyMin = 125
 cannyMax = 150
 borderCount = 220
 cap = cv2.VideoCapture(0)
 
-while(True):
 
+while(True):
     #MODIFY PARAMS BASED ON FEEDBACK
     if borderCount > 320:
-        cannyMin += 25   
+        cannyMin += 25
         cannyMax += 25
     if borderCount < 180:
         if cannyMin >= 25 and cannyMax >= 50:
-            cannyMin -= 25   
+            cannyMin -= 25
             cannyMax -= 25
 
-    # Capture frame-by-frame
     ret, frame = cap.read()
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayFrame = cv2.medianBlur(grayFrame,5)
+    grayFrame = cv2.adaptiveThreshold(grayFrame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+    grayFrame = cv2.medianBlur(grayFrame,5)
+
     #EDGE DETECTION
-    edges = cv2.Canny(gray,cannyMin,cannyMax,apertureSize = 3)
+    grayFrame = cv2.Canny(grayFrame,cannyMin,cannyMax,apertureSize = 3)
     #EDGE REDUCTION
-    edges = cv2.dilate(edges,None)
+    grayFrame = cv2.dilate(grayFrame,None)
 
-    #CONTOUR DETECTION
-    vals = edges.copy() #Make a clone because FindContours can modify the image
-    contours0, hierarchy=cv2.findContours(vals, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = [cv2.approxPolyDP(cnt, 3, True) for cnt in contours0]
+    #grayFrame = cv2.GaussianBlur(grayFrame,(5,5),0)
+    cv2.imshow('video gray', grayFrame)
+    cv2.imshow('video original', frame)
 
-    #GIVE FEEDBACK
-    borderCount = len(contours0)
-    if debug:
-        print (borderCount, cannyMin , cannyMax)
-
-    #DRAWING CONTOURS
-    _black = (0, 0, 0)
-    levels=2 #1 contours drawn, 2 internal contours as well, 3 ...
-    cv2.drawContours (frame, contours, (-1, 2)[levels <= 0], _black, 3, cv2.LINE_AA, hierarchy, abs(levels))
-
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-
-    #KEYBOARD CALLBACK
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q') or key == ord('Q'):
         break
+
+
 
 # When everything done, release the capture
 cap.release()
